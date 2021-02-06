82ce07f352d09b52fb00d2d6d7fd3021b97b6c51
Robert Mariuson <gigglefeet@gmail.com>
2021-02-06 17:29:30 +0100

    Add memescraper version 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './index.css';
+import Header from './Header';
+import MemeGen from './MemeGen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <MemeGen />
     </div>
   );
 }
+/*
+It should allow the user to:
 
+
+
+  download the meme*/
 export default App;
---
+++ src/MemeGen.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Button from './Button';
+
+export default function MemeGen() {
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [memes, setMemes] = useState([]);
+  const [img, setImg] = useState('aag');
+
+  // enter text for the top and bottom of the meme
+
+  const onChangeTopText = (e) => {
+    setTopText(e.target.value);
+  };
+  const onChangeBottomText = (e) => {
+    setBottomText(e.target.value);
+  };
+  const memeImg = `https://api.memegen.link/images/${img}/${topText}/${bottomText}`;
+
+  useEffect(() => {
+    axios
+      .get('https://api.memegen.link/templates/')
+      .then((res) => {
+        console.log(res);
+        setMemes(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <img className="MemeImage" src={memeImg} alt={memeImg.name} />
+      <select
+        id="selectImg"
+        onChange={() => {
+          const imgSelect = document.getElementById('selectImg');
+          const selectedValue = imgSelect.value;
+          setImg(selectedValue);
+        }}
+      >
+        {memes.map((meme) => (
+          <option key={meme.key} value={meme.key}>
+            {meme.name}
+          </option>
+        ))}
+      </select>
+
+      <div className="MemeText">
+        <label htmlFor="top">TopText</label>
+        <input type="text" value={topText} onChange={onChangeTopText} />
+        <br />
+        <label htmlFor="bottom">BottomText</label>
+        <input type="text" value={bottomText} onChange={onChangeBottomText} />
+        <br />
+      </div>
+      <div>
+        <Button memeImg={memeImg} />
+      </div>
+    </div>
+  );
+}
+
+// handling the onChange Event.
+
+// const [bottomText, setBottomText] = useState('');
+
+// function handleChange(e) {
+// console.log(e.target.value);
